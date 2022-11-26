316c0061148a8fe0eff0e3e3f42b4cdae86b695a
Richard Raya <rdxzv.dev@gmail.com>
2022-11-26 17:19:38 -0300

    fonts: Add MiLanPro
    Change-Id: Ib6c7d90a4905f653d654d28a646f036b8c382d38
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- overlay/overlays.mk
+++ overlay/overlays.mk
@@ -17,6 +17,7 @@ PRODUCT_PACKAGES += \
     FontInterOverlay \
     FontLinotteSourceOverlay \
     FontManropeOverlay \
+    FontMiLanProOverlay \
     FontOnePlusSansOverlay  \
     FontOppoSansOverlay \
     FontUrbanistOverlay \
