6bd0fc35e135709b53f55240bb487e005f2defd8
SH4ND <miqueiasantos20@gmail.com>
2024-01-14 14:17:21 -0300

    aospa: products: Introduce Redmi Note 8/8T (ginkgo)
    Change-Id: Ic94bbd089574d4f9a1d8169ea94e9ddae13f0abc
    Signed-off-by: SH4ND <miqueiasantos20@gmail.com>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -25,6 +25,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/dubai/aospa_dubai.mk \
     $(LOCAL_DIR)/fuxi/aospa_fuxi.mk \
     $(LOCAL_DIR)/ishtar/aospa_ishtar.mk \
+    $(LOCAL_DIR)/ginkgo/aospa_ginkgo.mk \
     $(LOCAL_DIR)/juice/aospa_juice.mk \
     $(LOCAL_DIR)/lahaina/aospa_lahaina.mk \
     $(LOCAL_DIR)/lisa/aospa_lisa.mk \
@@ -73,6 +74,7 @@ COMMON_LUNCH_CHOICES += \
     aospa_dubai-userdebug \
     aospa_fuxi-userdebug \
     aospa_ishtar-userdebug \
+    aospa_ginkgo-userdebug \
     aospa_juice-userdebug \
     aospa_lahaina-userdebug \
     aospa_lisa-userdebug \
